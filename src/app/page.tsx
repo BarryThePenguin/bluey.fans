4fa85cbd3dd725c8a9180d2c28c57036d6cd739f
Jonathan Haines <jonno.haines@gmail.com>
2023-11-10 06:41:36 +1100

    more copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,38 @@ export default function Home() {
   }
 
   return (
-    <main className="h-screen p-8 md:p-16 bg-no-repeat bg-cover bg-bottom bg- bg-[url('/grannies.jpg')] md:bg-[url('/family.webp')]">
-      <div className="p-4 md:p-8 space-y-6 rounded-2xl bg-slate-700/50 text-center">
-        <p className="text-5xl">
-          Lilly &amp; Arabella <span className="whitespace-nowrap">turn 2</span>
+    <main className="h-screen p-8 md:p-16 bg-no-repeat bg-cover bg-bottom bg-[url('/grannies.jpg')] md:bg-[url('/family.webp')]">
+      <div className="mx-auto max-w-md p-4 md:p-8 space-y-6 rounded-2xl bg-slate-900/60 text-center">
+        <p className="text-slate-50 text-3xl">Hi Hortle Family!</p>
+        <p className="text-slate-50 text-4xl">
+          Lilly &amp; Arabella are{" "}
+          <span className="whitespace-nowrap">turning 2</span>
         </p>
-        <p className="text-4xl">Can you make it?</p>
-        <form action={rsvp} className="space-x-6 text-3xl">
-          <button
-            className="rounded px-9 py-2 bg-cyan-500"
-            name="rsvp"
-            type="submit"
-            value="yes"
-          >
-            Yes
-          </button>
-          <button
-            className="rounded px-9 py-2 bg-indigo-500"
-            name="rsvp"
-            type="submit"
-            value="no"
-          >
-            No
-          </button>
+        <p className="text-slate-50 text-3xl">
+          We are having a party and you&apos;re invited
+        </p>
+        <p className="text-slate-50 text-3xl">Can you make it?</p>
+        <form action={rsvp} className=" text-2xl">
           <input name="name" type="hidden" value="my name" />
+
+          <fieldset className="space-x-6">
+            <button
+              className="rounded px-9 py-2 text-slate-50 bg-cyan-500"
+              name="rsvp"
+              type="submit"
+              value="yes"
+            >
+              Yes 🥳
+            </button>
+            <button
+              className="rounded px-9 py-2 text-slate-50 bg-indigo-500"
+              name="rsvp"
+              type="submit"
+              value="no"
+            >
+              No 😞
+            </button>
+          </fieldset>
         </form>
       </div>
     </main>
